15fe81d093f78fdab23838983351d21a64e22f0a
ivanff1980 <ivanff1980@hotmail.com>
2023-07-16 16:24:26 +0200

    Test django

--- PickStatsGal/myapp/views.py
+++ PickStatsGal/myapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from django.http import FileResponse
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    ##return HttpResponse("Hello, world!")
+    ##print ("Hello, world!")
+    return FileResponse(open("../PickStatsGal1.jpeg", "rb"))
